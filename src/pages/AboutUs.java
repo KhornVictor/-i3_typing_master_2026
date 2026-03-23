3e3460b659a1186ced6c6cf8cb25e0c7768ebe2d
Bunnaka <benbunnaka@gmail.com>
2026-03-23 13:28:14 +0700

    update

---
+++ src/pages/AboutUs.java
@@ -0,0 +1,9 @@
+package pages;
+
+class AboutScreen {
+    void show() {
+        System.out.println("Typing Test System");
+        System.out.println("Developed by IT students");
+        System.out.println("Version 1.0");
+    }
+}
